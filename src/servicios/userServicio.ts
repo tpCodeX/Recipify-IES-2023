a46c603555b239a598286f85bccc3fdff8e297d4
Geronimo Luminari <geronimoluminari3@outlook.com>
2023-09-02 20:10:15 -0300

    cambios en servicios

--- src/servicios/userServicio.ts
+++ src/servicios/userServicio.ts
@@ -1,5 +1,6 @@
 import {prisma} from '@/libs/prisma'
 import { compare } from 'bcrypt';
+import { hash } from 'bcrypt';
 class UsuarioServicio {
     constructor() {
         
@@ -16,9 +17,31 @@ class UsuarioServicio {
         if (!userExist) {
             throw new Error("El mail ingresado no se ha podido encontrar.");
         }
-        
+
     }
-    
-    
+    async comparePassword(password:string,hashedPass:string){
+        return await compare(password,hashedPass)
+    }
+    async hashPassword(pass:string){
+        return await hash(pass,10)
+    };
+    async usuarioExiste({userName,email}:IUser){
+
+            const userByName= await prisma.user.findFirst({
+                where:{
+                    userName: userName
+                }
+            });
+            const userByMail= await prisma.user.findFirst({
+                where:{
+                    email:email
+                }
+            })
+
+            const nameExiste= userByName? true:false
+            const mailExiste= userByMail? true:false
+
+            return{nameExiste,mailExiste}
+    }
 
 }
