1b838d8f89026ec6176eff9e645e668dcad8dce6
tpCodeX <ponce.code.mza@gmail.com>
2023-09-04 01:27:45 -0300

    add public/images folder & creating navbar & app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import NavBar from "@/components/navbar-component/NavBar";
 import Menu from "@/components/menu-component/Menu";
-import RecipeSection from "@/components/recipe-component/RecipeSection";
+// import RecipeSection from "@/components/recipe-component/RecipeSection";
 import './normalize.css'
 import './layout.css'
 import Galeria from "@/components/galeria-component/Galeria";
