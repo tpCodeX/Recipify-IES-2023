0cb42151cd48f507fb2a81f43594fcfcfac235c5
tpCodeX <ponce.code.mza@gmail.com>
2023-09-18 16:38:27 -0300

    del unused expressions

--- src/app/recetas/layout.tsx
+++ src/app/recetas/layout.tsx
@@ -1,13 +1,9 @@
-import Galeria from "@/components/galeria-component/Galeria"
-import Menu from "@/components/menu-component/Menu"
 
 
 
 const RecetasLayout =({children}:{children:React.ReactNode})=>{
     return <>
-        {/* <Menu/> */}
         {children}
-        {/* <Galeria/> */}
         </>
 }
 
