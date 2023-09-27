d1617176c5c3883faac06917ba1fa176fd65760c
tpCodeX <ponce.code.mza@gmail.com>
2023-09-26 23:32:42 -0300

    eliminacion de metadata en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 
 import { Providers } from "./providers";
-import type { Metadata } from "next";
 import './normalize.css'
 import './layout.css'
 import './globals.css'
 import NavBar from "@/components/navbar-component/NavBar";
 
-export const metadata: Metadata = {
-  title: 'Recipify 👨🏻‍🍳',
-  description: 'Recipify is a simple recipe-finding application. Enjoy it! :)',
-}
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
