3d9ee1a75031c9f5e911680de950408381af3cca
tpCodeX <ponce.code.mza@gmail.com>
2023-10-05 15:49:05 -0300

    añadiendo ruta de registro y componente de registro de la rama feature/geronimo/frontRecetas

---
+++ src/app/register/page.tsx
@@ -0,0 +1,14 @@
+
+
+import RegisterComponent from '@/components/register/registerComponent'
+import React from 'react'
+
+function Register() {
+  return (
+   <>
+   <RegisterComponent/>
+   </>
+  )
+}
+
+export default Register
